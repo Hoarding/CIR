7b49117adc116ffa1a9270e415c6fa4b4c0f4627
Sophie Lehar <sophielehar@wireless1x-155-41-97-82.bu.edu>
2014-02-12 18:11:50 -0500

    Initial Commit

---
+++ Clutter Image Rating/gaiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  gaiAppDelegate.h
+//  Clutter Image Rating
+//
+//  Created by Sophie Lehar on 2/12/14.
+//  Copyright (c) 2014 Clutter Image Rating App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gaiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Clutter Image Rating/gaiViewController.m
@@ -0,0 +1,29 @@
+//
+//  gaiViewController.m
+//  Clutter Image Rating
+//
+//  Created by Sophie Lehar on 2/12/14.
+//  Copyright (c) 2014 Clutter Image Rating App. All rights reserved.
+//
+
+#import "gaiViewController.h"
+
+@interface gaiViewController ()
+
+@end
+
+@implementation gaiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
